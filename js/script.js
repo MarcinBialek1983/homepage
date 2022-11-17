3e8adfeec6938286c76fd1df9a7ffea5b170d33a
Marcin Bialek <test-git@youcode.pl>
2022-11-17 09:58:54 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+console.log("Cześć Wszystkim!");
+let button = document.querySelector(".button");
+let body = document.querySelector(".body");
+let themeName = document.querySelector(".themeName");
+
+button.addEventListener("click", () => {
+    body.classList.toggle("body--dark");
+
+    if (body.classList.contains("body--dark")) {
+        themeName.innerText = "jasne";
+    } else {
+        themeName.innerText = "ciemne";
+    }
+});
+
+
